2b24158b2ed6dbb179e2c92425cd3624ffdd154b
Alex Sullivan <alexsullivan114@gmail.com>
2018-08-12 10:19:49 -0400

    Ship the tutorial with the database class to start

---
+++ app/src/main/java/android/raywenderlich/com/RedditClone/database/RedditDb.kt
@@ -0,0 +1,21 @@
+package android.raywenderlich.com.RedditClone.database
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import android.raywenderlich.com.RedditClone.networking.RedditPost
+
+@Database(
+  entities = [RedditPost::class],
+  version = 1,
+  exportSchema = false
+)
+abstract class RedditDb : RoomDatabase() {
+  companion object {
+    fun create(context: Context): RedditDb {
+      val databaseBuilder = Room.databaseBuilder(context, RedditDb::class.java, "reddit.db")
+      return databaseBuilder.build()
+    }
+  }
+}
