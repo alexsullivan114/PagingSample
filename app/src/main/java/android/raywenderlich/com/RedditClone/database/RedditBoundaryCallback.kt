ad8e6fa6115132363f470522ed0bfa3154d3f523
Alex Sullivan <alexsullivan114@gmail.com>
2018-08-12 10:36:24 -0400

    Add mroe params to boundary callback

--- app/src/main/java/android/raywenderlich/com/RedditClone/database/RedditBoundaryCallback.kt
+++ app/src/main/java/android/raywenderlich/com/RedditClone/database/RedditBoundaryCallback.kt
@@ -3,10 +3,14 @@ package android.raywenderlich.com.RedditClone.database
 import android.arch.paging.PagedList
 import android.raywenderlich.com.RedditClone.networking.RedditPost
 import android.raywenderlich.com.RedditClone.networking.RedditService
+import android.raywenderlich.com.RedditClone.utils.PagingRequestHelper
+import java.util.concurrent.Executors
 
 class RedditBoundaryCallback(private val db: RedditDb): PagedList.BoundaryCallback<RedditPost>() {
 
   private val api = RedditService.createService()
+  private val executor = Executors.newSingleThreadExecutor()
+  private val helper = PagingRequestHelper(executor)
 
   override fun onZeroItemsLoaded() {
     super.onZeroItemsLoaded()
